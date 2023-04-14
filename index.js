97435e01e03f907eda5b5fbb7fd425c2490227cb
YashDalwani <yashdalwani21@gmail.com>
2023-04-14 11:23:54 +0530

    commit 2

---
+++ index.js
@@ -0,0 +1,7 @@
+const inputFieldEl = document.getElementById("input_field")
+const addButtonEl = document.getElementById("add_button")
+
+addButtonEl.addEventListener("click", function(){
+    let inputValue = inputFieldEl.value
+    console.log(inputValue)
+})
